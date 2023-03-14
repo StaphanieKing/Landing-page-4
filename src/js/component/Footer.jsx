3c0be0d7046f42e01b21ebdf9eacf8a3d54e1642
StaphanieKing <staphaniek@yahoo.com>
2023-03-14 04:19:07 +0000

    finished cards

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Footer=()=>{
+return(
+		
+<div className='sticky-lg-bottom bg-dark'>
+    <div className= 'footer-body'>
+        //<h5>Copyright @ Loretta Mayumi Thompson 2022</h5>
+        <p className="footer-text">Copyright @ Loretta Mayumi Thompson 2022</p>
+    </div>
+
+</div>	
+    
+
+ 
+
+
+);
+
+};
+export default Footer;
